4e1e866024f2cf05748d30bbf2c9bab8deeda7d2
sholmp <yuppie@yoko.com>
2021-11-06 23:56:00 +0100

    first refactor, actually using visitor pattern now

--- print_tree.py
+++ print_tree.py
@@ -6,57 +6,93 @@ class NodeStyle(Enum):
     BULLET = 2
     TREE = 3
 
+
+
 class PrintTree(Visitor):
-    def __init__(self, node_style):
-        self.node_style = node_style
+    def __init__(self, node_style: NodeStyle):
         if node_style == NodeStyle.INDENT:
-            self.prefix_token = ''
+            self.visitorFunction = self.printWithIndent
         elif node_style == NodeStyle.BULLET:
-            self.prefix_token = '* '
+            self.visitorFunction = self.printWithBullet
 
-    def traverse(self, start_node: Node):
+    def printWithIndent(self, node: Node):
+        self.output_string += '  ' * node.depth() + node.name + '\n'
+    
+    def printWithBullet(self, node: Node):
+        self.output_string += '  ' * node.depth() + '* ' + node.name + '\n'
+
+    def traverse(self, node: Node):
         self.output_string = ""
-        self.traverseInternal(start_node)
-        self.output_string = self.output_string[:-1] # strip final new line to pass testcase
-        return self.output_string
+        self.traverseInternal(node)
+        return self.output_string[:-1]
 
-    def traverseInternal(self, start_node):
-        self.output_string += '  ' * start_node.depth() + self.prefix_token + start_node.name + '\n'
-
-        if type(start_node) == Leaf:
+    def traverseInternal(self, node: Node):
+        node.accept(self)
+        if type(node) == Leaf:
             return
-        for child in start_node.children:
+        for child in node.children:
             self.traverseInternal(child)
 
-    def forwarder(self, node):
-        self.output_string = ""
-        self.traverseFolderStructureWay(node)
-        return self.output_string
+    def visit(self, node: Node):
+        self.visitorFunction(node)
 
-    # not quite working... yet!
-    def traverseFolderStructureWay(self, node : Node):
-        if node.depth() > 1:
-            for i in range(1,node.depth()):
-                self.output_string += " │  "
+# def printNodeNameNothingElse(node: Node):
+#     print(node.name)
 
-        if type(node) == Leaf:
-            if node.parent == None: #
-                self.output_string += f"─╼ {node.name}"
-            elif node.parent.children[-1] == node: # last element in current branch
-                self.output_string += f"└─╼ {node.name}\n"
-            else: # we must be a leaf node on the middle of a branch
-                self.output_string += f" ├─╼ {node.name}\n"
-            return
-        else: # we are a regular node
-            if node.isRoot():
-                self.output_string += f" ╿ {node.name}\n"
-            elif node.parent.children[-1] == node: # last element in current branch
-                self.output_string += f" └─┮ {node.name}\n"
-            else: # regular node middle of a branch
-                self.output_string += f" ├─┮ {node.name}\n"
+# class PrintTree(Visitor):
+#     def __init__(self, node_style):
+#         self.node_style = node_style
+#         if node_style == NodeStyle.INDENT:
+#             self.prefix_token = ''
+#         elif node_style == NodeStyle.BULLET:
+#             self.prefix_token = '* '
 
-        for child in node.children:
-            self.traverseFolderStructureWay(child)
+#     def traverse(self, start_node: Node):
+#         self.output_string = ""
+#         self.traverseInternal(start_node)
+#         self.output_string = self.output_string[:-1] # strip final new line to pass testcase
+#         return self.output_string
+
+#     def traverseInternal(self, start_node):
+#         self.output_string += '  ' * start_node.depth() + self.prefix_token + start_node.name + '\n'
+
+#         if type(start_node) == Leaf:
+#             return
+#         for child in start_node.children:
+#             self.traverseInternal(child)
+
+#     def forwarder(self, node):
+#         self.output_string = ""
+#         self.traverseFolderStructureWay(node)
+#         return self.output_string
+
+#     # not quite working... yet!
+#     def traverseFolderStructureWay(self, node : Node):
+#         if node.depth() > 1:
+#             for i in range(1,node.depth()):
+#                 self.output_string += " │  "
+
+#         if type(node) == Leaf:
+#             if node.parent == None: #
+#                 self.output_string += f"─╼ {node.name}"
+#             elif node.parent.children[-1] == node: # last element in current branch
+#                 self.output_string += f"└─╼ {node.name}\n"
+#             else: # we must be a leaf node on the middle of a branch
+#                 self.output_string += f" ├─╼ {node.name}\n"
+#             return
+#         else: # we are a regular node
+#             if node.isRoot():
+#                 self.output_string += f" ╿ {node.name}\n"
+#             elif node.parent.children[-1] == node: # last element in current branch
+#                 self.output_string += f" └─┮ {node.name}\n"
+#             else: # regular node middle of a branch
+#                 self.output_string += f" ├─┮ {node.name}\n"
+
+#         for child in node.children:
+#             self.traverseFolderStructureWay(child)
+
+
+
 
 
 if __name__ == "__main__":
@@ -64,10 +100,11 @@ if __name__ == "__main__":
     tree = Node("Scene", Leaf("Table"), Leaf("Object"))
     tree = Node("Scene", Node("Robot", Node("Flange", Node("Gripper", Leaf("Object"))), Leaf("Camera")), Node("Table", Leaf("Box")))
 
-    pt = PrintTree(NodeStyle.TREE)
+    pt = PrintTree(NodeStyle.INDENT)
 
-    output = pt.forwarder(tree)
-    print(output)
+    print(pt.traverse(tree))
+
+
 
  
 
--- tree.py
+++ tree.py
@@ -1,5 +1,7 @@
 
+from abc import abstractmethod
 from typing import Type
+import copy
 
 
 class Node():
@@ -15,8 +17,8 @@ class Node():
             self.children.append(child)
             child.parent = self
 
-    def accept(self):
-        pass
+    def accept(self, visitor):
+        visitor.visit(self)
 
     def depth(self):
         depth = 0
@@ -36,14 +38,57 @@ class Leaf(Node):
         self.name = name
 
 class Visitor():
-    def traverse(self):
-        pass
+    @abstractmethod
+    def visit(self, node: Node):
+        raise NotImplementedError
 
-    def visit(self):
-        pass
+    @abstractmethod
+    def traverse(self, node: Node):
+        node.accept(self)
+        if type(node) == Leaf:
+            return
+        for child in node.children:
+            self.traverse(child)
+
+
+from enum import Enum 
+
+# class PrintTree(Visitor):
+#     def __init__(self, node_style: NodeStyle):
+#         if node_style == NodeStyle.INDENT:
+#             self.visitorFunction = self.printWithIndent
+#         elif node_style == NodeStyle.BULLET:
+#             self.visitorFunction = self.printWithBullet
+
+#     def printWithIndent(self, node: Node):
+#         self.output_string += '  ' * node.depth() + node.name + '\n'
+    
+#     def printWithBullet(self, node: Node):
+#         self.output_string += '  ' * node.depth() + '* ' + node.name + '\n'
+
+#     def traverse(self, node: Node):
+#         self.output_string = ""
+#         self.traverseInternal(node)
+#         return self.output_string[:-1]
+
+#     def traverseInternal(self, node: Node):
+#         node.accept(self)
+#         if type(node) == Leaf:
+#             return
+#         for child in node.children:
+#             self.traverseInternal(child)
+
+#     def visit(self, node: Node):
+#         self.visitorFunction(node)
+
+
 
 if __name__ == "__main__":
     n = Node("Scene", Node("Robot", Node("Flange", Node("Gripper", Leaf("Object"))), Leaf("Camera")), Node("Table", Leaf("Box")))
 
-    for child in n.children:
-        print(child.name)
+    v = PrintTree(NodeStyle.INDENT)
+    v = PrintTree(NodeStyle.BULLET)
+    print(v.traverse(n))
+
+    # pt = PrintTree()
+    # pt.traverse(n)
