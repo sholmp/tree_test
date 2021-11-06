36d30396c020020225c9045c9c8ac0382193a375
sholmp <yuppie@yoko.com>
2021-11-06 23:58:29 +0100

    small cleanup

--- tree.py
+++ tree.py
@@ -50,45 +50,5 @@ class Visitor():
         for child in node.children:
             self.traverse(child)
 
-
-from enum import Enum 
-
-# class PrintTree(Visitor):
-#     def __init__(self, node_style: NodeStyle):
-#         if node_style == NodeStyle.INDENT:
-#             self.visitorFunction = self.printWithIndent
-#         elif node_style == NodeStyle.BULLET:
-#             self.visitorFunction = self.printWithBullet
-
-#     def printWithIndent(self, node: Node):
-#         self.output_string += '  ' * node.depth() + node.name + '\n'
-    
-#     def printWithBullet(self, node: Node):
-#         self.output_string += '  ' * node.depth() + '* ' + node.name + '\n'
-
-#     def traverse(self, node: Node):
-#         self.output_string = ""
-#         self.traverseInternal(node)
-#         return self.output_string[:-1]
-
-#     def traverseInternal(self, node: Node):
-#         node.accept(self)
-#         if type(node) == Leaf:
-#             return
-#         for child in node.children:
-#             self.traverseInternal(child)
-
-#     def visit(self, node: Node):
-#         self.visitorFunction(node)
-
-
-
 if __name__ == "__main__":
-    n = Node("Scene", Node("Robot", Node("Flange", Node("Gripper", Leaf("Object"))), Leaf("Camera")), Node("Table", Leaf("Box")))
-
-    v = PrintTree(NodeStyle.INDENT)
-    v = PrintTree(NodeStyle.BULLET)
-    print(v.traverse(n))
-
-    # pt = PrintTree()
-    # pt.traverse(n)
+    pass
